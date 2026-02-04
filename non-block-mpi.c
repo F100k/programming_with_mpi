f6925a90b89a1911868731983c239c9638fe7f83
jatup <jatup@debian.jatup>
2026-02-04 22:25:13 +0700

    fix bug

--- non-block-mpi.c
+++ non-block-mpi.c
@@ -8,7 +8,7 @@ int read_matrix_flat(const char *filename, float **matrix, int *rows, int *cols)
     if (!fp) return -1;
 
     fscanf(fp, "%d %d", rows, cols);
-    *matrix = (float *)malloc((*rows) * (*cols) * sizeof(float));
+    *matrix = malloc((*rows) * (*cols) * sizeof(float));
 
     for (int i = 0; i < (*rows) * (*cols); i++)
         fscanf(fp, "%f", &((*matrix)[i]));
@@ -17,8 +17,7 @@ int read_matrix_flat(const char *filename, float **matrix, int *rows, int *cols)
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -27,28 +26,26 @@ int main(int argc, char *argv[])
     int rows = 0, cols = 0;
     float *A = NULL, *B = NULL, *C = NULL, *Result = NULL;
 
+    MPI_Request reqs[4];
     double start = MPI_Wtime();
 
     if (rank == 0) {
-        if (read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025/matAsmall.txt", &A, &rows, &cols) != 0 ||
-            read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025/matBsmall.txt", &B, &rows, &cols) != 0 ||
-            read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025/solutionsmall.txt", &Result, &rows, &cols) != 0) {
-            printf("error address\n");
-            MPI_Abort(MPI_COMM_WORLD, 1);
-        }
+        read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025matAsmall.txt", &A, &rows, &cols);
+        read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025matBsmall.txt", &B, &rows, &cols);
+        read_matrix_flat("/home/jatup/Desktop/HPC/LAB_lecture2_2025solutionsmall.txt", &Result, &rows, &cols);
 
         for (int p = 1; p < size; p++) {
-            MPI_Send(&rows, 1, MPI_INT, p, 10, MPI_COMM_WORLD);
-            MPI_Send(&cols, 1, MPI_INT, p, 11, MPI_COMM_WORLD);
+            MPI_Isend(&rows, 1, MPI_INT, p, 10, MPI_COMM_WORLD, &reqs[0]);
+            MPI_Isend(&cols, 1, MPI_INT, p, 11, MPI_COMM_WORLD, &reqs[1]);
         }
     } else {
-        MPI_Recv(&rows, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&cols, 1, MPI_INT, 0, 11, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Irecv(&rows, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, &reqs[0]);
+        MPI_Irecv(&cols, 1, MPI_INT, 0, 11, MPI_COMM_WORLD, &reqs[1]);
+        MPI_Waitall(2, reqs, MPI_STATUSES_IGNORE);
     }
 
     int base = rows / size;
     int rem  = rows % size;
-
     int local_rows = base + (rank < rem ? 1 : 0);
     int local_size = local_rows * cols;
 
@@ -58,24 +55,22 @@ int main(int argc, char *argv[])
 
     if (rank == 0) {
         int offset = local_rows;
-
         for (int p = 1; p < size; p++) {
             int rows_p = base + (p < rem ? 1 : 0);
-            int count  = rows_p * cols;
+            int count = rows_p * cols;
 
-            MPI_Send(A + offset * cols, count, MPI_FLOAT, p, 20, MPI_COMM_WORLD);
-            MPI_Send(B + offset * cols, count, MPI_FLOAT, p, 21, MPI_COMM_WORLD);
-
+            MPI_Isend(A + offset * cols, count, MPI_FLOAT, p, 20, MPI_COMM_WORLD, &reqs[0]);
+            MPI_Isend(B + offset * cols, count, MPI_FLOAT, p, 21, MPI_COMM_WORLD, &reqs[1]);
             offset += rows_p;
         }
-
         for (int i = 0; i < local_size; i++) {
             A_local[i] = A[i];
             B_local[i] = B[i];
         }
     } else {
-        MPI_Recv(A_local, local_size, MPI_FLOAT, 0, 20, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(B_local, local_size, MPI_FLOAT, 0, 21, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Irecv(A_local, local_size, MPI_FLOAT, 0, 20, MPI_COMM_WORLD, &reqs[0]);
+        MPI_Irecv(B_local, local_size, MPI_FLOAT, 0, 21, MPI_COMM_WORLD, &reqs[1]);
+        MPI_Waitall(2, reqs, MPI_STATUSES_IGNORE);
     }
 
     for (int i = 0; i < local_size; i++)
@@ -83,49 +78,28 @@ int main(int argc, char *argv[])
 
     if (rank == 0) {
         C = malloc(rows * cols * sizeof(float));
+        int offset = local_rows;
 
         for (int i = 0; i < local_size; i++)
             C[i] = C_local[i];
 
-        int offset = local_rows;
-
         for (int p = 1; p < size; p++) {
             int rows_p = base + (p < rem ? 1 : 0);
-            int count  = rows_p * cols;
-
-            MPI_Recv(C + offset * cols, count,
-                     MPI_FLOAT, p, 30, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+            int count = rows_p * cols;
+            MPI_Irecv(C + offset * cols, count, MPI_FLOAT, p, 30, MPI_COMM_WORLD, &reqs[0]);
+            MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
             offset += rows_p;
         }
 
-        int mismatch = 0;
-        for (int i = 0; i < rows * cols; i++)
-            if (fabsf(C[i] - Result[i]) > 1e-5f)
-                mismatch++;
-
-        if (mismatch == 0)
-            printf("Block Done\n");
-        else
-            printf("Blocking MPI : mismatches = %d\n", mismatch);
+        printf("Non-blocking MPI Done\n");
     } else {
-        MPI_Send(C_local, local_size, MPI_FLOAT, 0, 30, MPI_COMM_WORLD);
+        MPI_Isend(C_local, local_size, MPI_FLOAT, 0, 30, MPI_COMM_WORLD, &reqs[0]);
+        MPI_Wait(&reqs[0], MPI_STATUS_IGNORE);
     }
 
     double end = MPI_Wtime();
     if (rank == 0)
-        printf("Execution time: %f seconds\n", end - start);
-
-    free(A_local);
-    free(B_local);
-    free(C_local);
-
-    if (rank == 0) {
-        free(A);
-        free(B);
-        free(C);
-        free(Result);
-    }
+        printf("Non-blocking time: %f sec\n", end - start);
 
     MPI_Finalize();
     return 0;
